afce72384d5f654323ac11e4f0ba4f6f2964e8c5
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-11-12 12:00:23 +0200

    Tehtävä 2.14 tehty

--- Section_2/maiden-tiedot/src/Components/countries.js
+++ Section_2/maiden-tiedot/src/Components/countries.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Weather = ({ countryInfo, weather }) => {
+    return (
+        <div>
+            <h2>Weather in {countryInfo.capital}</h2>
+            <p>Temperature: {weather.current.temperature} &#8451;</p>
+            <img src={weather.current.weather_icons[0]} alt='Weather icon' width='100px' />
+            <p>Wind: {weather.current.wind_speed} mph / Direction: {weather.current.wind_dir}</p>
+        </div>
+    )
+}
+
+const CountryInfo = ({ countryInfo }) => {
+    const [weather, setWeather] = useState(null);
+    useEffect(() => {
+        const api_key = process.env.REACT_APP_API_KEY
+        axios.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${countryInfo.capital}`)
+            .then(response => {
+                setWeather(response.data)
+                console.log(response.data)
+            })
+    }, [countryInfo])
+
+    return (
+        <div>
+            <h1>{countryInfo.name}</h1>
+            <div>
+                <p>Capital: {countryInfo.capital}</p>
+                <p>Population: {countryInfo.population}</p>
+            </div>
+            <div>
+                <h2>Languages</h2>
+                <ul>
+                    {countryInfo.languages.map(language => <li key={language.name}>{language.name}</li>)}
+                </ul>
+            </div>
+            <div>
+                <img src={countryInfo.flag} alt='Flag' width='200px' />
+            </div>
+            {weather ? <Weather countryInfo={countryInfo} weather={weather} /> : null}
+        </div>
+    )
+}
+
+const ShowCountryInfoBtn = () => {
+    return (
+        <button>
+            Show
+        </button>
+    )
+}
 
 const Countries = ({ countries }) => {
-    console.log(countries.length);
     if (countries.length > 10) {
         return (
             <div>
@@ -10,30 +61,18 @@ const Countries = ({ countries }) => {
         );
     } else if (countries.length === 1) {
         const countryInfo = countries.reduce(country => country)
-        console.log(countryInfo);
         return (
-            <div>
-                <h1>{countryInfo.name}</h1>
-                <div>
-                    <p>Capital: {countryInfo.capital}</p>
-                    <p>Population: {countryInfo.population}</p>
-                </div>
-                <div>
-                    <h2>Languages</h2>
-                    <ul>
-                        {countryInfo.languages.map(language => <li key={language.name}>{language.name}</li>)}
-                    </ul>
-                </div>
-                <div>
-                    <img src={countryInfo.flag} alt='Flag' width='200px'/>
-                </div>
-            </div>
+            <CountryInfo countryInfo={countryInfo} />
         )
     } else {
         return (
             <div>
                 <ul>
-                    {countries.map(country => <li key={country.name}>{country.name}</li>)}
+                    {countries.map(country =>
+                        <li key={country.name}>
+                            {country.name} <ShowCountryInfoBtn />
+                        </li>
+                    )}
                 </ul>
             </div>
         );
