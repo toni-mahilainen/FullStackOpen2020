991f94b7aeb123429f85a3b268997b09d57432f5
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-12-10 10:29:13 +0200

    Tehtävät 7.1 ja 7.2 tehty

---
+++ Section_7/routed-anecdotes/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import {
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useParams,
+    useHistory,
+    useRouteMatch
+} from 'react-router-dom'
+import About from './Components/About'
+import Anecdote from './Components/Anecdote'
+import AnecdoteList from './Components/AnecdoteList'
+import CreateNew from './Components/CreateNew'
+import Footer from './Components/Footer'
+import Menu from './Components/Menu'
+
+const App = () => {
+    const [anecdotes, setAnecdotes] = useState([
+        {
+            content: 'If it hurts, do it more often',
+            author: 'Jez Humble',
+            info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+            votes: 0,
+            id: '1'
+        },
+        {
+            content: 'Premature optimization is the root of all evil',
+            author: 'Donald Knuth',
+            info: 'http://wiki.c2.com/?PrematureOptimization',
+            votes: 0,
+            id: '2'
+        }
+    ])
+    const [notification, setNotification] = useState('')
+
+    const addNew = (anecdote) => {
+        anecdote.id = (Math.random() * 10000).toFixed(0)
+        setAnecdotes(anecdotes.concat(anecdote))
+    }
+
+    const anecdoteById = (id) =>
+        anecdotes.find(a => a.id === id)
+
+    const vote = (id) => {
+        const anecdote = anecdoteById(id)
+
+        const voted = {
+            ...anecdote,
+            votes: anecdote.votes + 1
+        }
+
+        setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+    }
+
+    return (
+        <div>
+            <h1>Software anecdotes</h1>
+            <Menu />
+            <Switch>
+                <Route path='/anecdote/:id'>
+                    <Anecdote anecdotes={anecdotes} />
+                </Route>
+                <Route exact path='/'>
+                    <AnecdoteList anecdotes={anecdotes} />
+                </Route>
+                <Route path='/create'>
+                    <CreateNew addNew={addNew} />
+                </Route>
+                <Route path='/about'>
+                    <About />
+                </Route>
+            </Switch>
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
