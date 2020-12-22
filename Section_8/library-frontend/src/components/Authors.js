783a5d34794c218758e73be48d6c6e8817de1a64
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-12-22 10:53:59 +0200

    Tehtävät 8.8 - 8-12 tehty

---
+++ Section_8/library-frontend/src/components/Authors.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import EditAuthorForm from './EditAuthorForm'
+
+const Authors = ({ show, authors }) => {
+    if (!show) {
+        return null
+    }
+
+    return (
+        <div>
+            <h2>authors</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>
+                            Born
+                        </th>
+                        <th>
+                            Books
+                        </th>
+                    </tr>
+                    {authors.map(a =>
+                        <tr key={a.name}>
+                            <td>{a.name}</td>
+                            <td>{a.born}</td>
+                            <td>{a.bookCount}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            <EditAuthorForm authors={authors}/>
+        </div>
+    )
+}
+
+export default Authors
---
+++ Section_8/library-frontend/src/components/Books.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Books = ({ show, books }) => {
+    if (!show) {
+        return null
+    }
+
+    return (
+        <div>
+            <h2>books</h2>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>
+                            Author
+                        </th>
+                        <th>
+                            Published
+                        </th>
+                    </tr>
+                    {books.map(a =>
+                        <tr key={a.title}>
+                            <td>{a.title}</td>
+                            <td>{a.author}</td>
+                            <td>{a.published}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Books
