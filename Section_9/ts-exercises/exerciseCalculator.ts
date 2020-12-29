b34b8bb77f31e3ded262d110ff676f21e890d956
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-12-29 19:35:15 +0200

    Parannuksia tehtävään 9.3

--- Section_9/ts-exercises/exerciseCalculator.ts
+++ Section_9/ts-exercises/exerciseCalculator.ts
@@ -8,12 +8,15 @@ interface Results {
     average: number
 }
 
-const calculateExercise = (hours: string[], target: number): Results => {
-    console.log('hours', hours);
-    console.log('target', target);
+const calculateExercise = (args: string[]): Results => {
+    if (args.length < 4) throw new Error('Not enough arguments')
+    
+    const hours = args.slice(2, args.length - 1);
+    const target = Number(args.pop());
 
     let ratingDescription = '';
     let rating = 0;
+    
     const periodLength = hours.length;
     const average = hours.map(value => Number(value)).reduce((acc, curr) => acc + curr) / hours.length;
     const trainingDays = hours.filter(value => Number(value) > 0).length;
@@ -22,27 +25,27 @@ const calculateExercise = (hours: string[], target: number): Results => {
     switch (true) {
         case average <= 1:
             ratingDescription = 'That was quite lazy job dude...';
-            rating = 1
+            rating = 1;
             break;
 
         case average > 1 && average <= 2:
             ratingDescription = 'Better but still not enough..';
-            rating = 1.5
+            rating = 1.5;
             break;
 
         case average > 2 && average <= 3:
             ratingDescription = 'Good job! Next week will be even better, right?';
-            rating = 2
+            rating = 2;
             break;
 
         case average > 3 && average <= 4:
             ratingDescription = 'Execellent job!... but you can still do it even better.';
-            rating = 2.5
+            rating = 2.5;
             break;
 
         case average > 4:
             ratingDescription = 'Super!';
-            rating = 3
+            rating = 3;
             break;
 
         default:
@@ -61,7 +64,7 @@ const calculateExercise = (hours: string[], target: number): Results => {
 }
 
 try {
-    console.log(calculateExercise(process.argv.slice(2, process.argv.length - 1), Number(process.argv.pop())));
+    console.log(calculateExercise(process.argv));
 } catch (error) {
     console.log(`Some errors! Message:\r\n${error.message}`);
 }
